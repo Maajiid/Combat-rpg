f1d804bf2961ca934267d12d76bb6e09d3bc604a
Maajiid <149937137+Maajiid@users.noreply.github.com>
2023-12-18 08:09:17 +0100

    Update niveau projet.py

--- niveau projet.py
+++ niveau projet.py
@@ -3,5 +3,5 @@ if Héros.EXP == 100:
         Héros.HP *= 1.1
         Héros.DEF *= 1.1
         Héros.DMG *= 1.1
-        Héros.EXP = 0
+        Héros.EXP -= 100
         print(f"Vous avez atteint le niveau {Héros.lvl}! Vos statistique ont augmenter")
